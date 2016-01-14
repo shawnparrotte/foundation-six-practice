39bc2255836aad8216df6caf21ffc500abe6df88
Shawn Parrotte <shawnparrotte@gmail.com>
2016-01-14 15:37:53 -0500

    adds responsive layout

--- js/app.js
+++ js/app.js
@@ -1,7 +1,27 @@
 $(document).foundation();
 
+//link border effect
+
 $(".links a").mouseenter(function(){
   $(this).css("border-bottom", "2px solid #0091CC");
 }).mouseleave(function(){
   $(this).css("border-bottom", "0 solid #0091CC")
-})
+});
+
+//jQuery Slider
+
+$('#slider-1').bxSlider({
+    maxSlides: 1,
+    speed: 2000,
+    auto: true
+  });
+
+//side panel menu
+
+var jPM = $.jPanelMenu({
+    menu: '#jmenu',
+    trigger: '#hamburger',
+    direction: "right",
+});
+
+jPM.on();
